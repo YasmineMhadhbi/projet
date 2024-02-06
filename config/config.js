894c8f036577c27d271bdead754fde91974331b7
imed jadli <imedjadli@bigup-conseils.com>
2024-02-06 14:26:27 +0100

    database configuration

---
+++ config/config.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const fs = require("fs");
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || "postgres",
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || "postgres",
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || "postgres",
+    dialectOptions: {
+      bigNumberStrings: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+require("dotenv").config();
+
 import { AppModule } from './app.module';
+const db = require('../models');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
 }
 bootstrap();
